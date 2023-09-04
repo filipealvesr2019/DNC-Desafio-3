56545f944e2b5a61f54b0d27bc7f9b148abe9ba8
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2023-09-04 18:35:43 -0300

    todo

--- todo/src/components/custumModal.jsx
+++ todo/src/components/custumModal.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import styles from '../components/custumModal.module.css'
 function custumModal({isOpen, onClose, onConfirm}) {
     if(!isOpen){
         return null;
     }
   return (
-    <div className="modal-overlay">
-        <div className="modal-content">
+    <div className={styles.modalOverlay}>
+        <div className={styles.modalContent}>
         <p>Você quer excluir a tarefa?</p>
-        <button onClick={onConfirm}>Sim</button>
-        <button onClick={onClose}>Não</button>
+        <p>Colocar as descrições das tarefas aqui.</p>
+        <div className={styles.btn}>
+        <button className={styles.btn} onClick={onClose}>Não</button>
+        <button className={styles.btn} onClick={onConfirm}>Sim</button>
+        </div>
+       
 
         </div>
     </div>
