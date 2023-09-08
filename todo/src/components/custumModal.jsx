54e5928d2240758062be19d1079484ae2355893e
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-09-08 13:20:11 -0300

    todo

--- todo/src/components/custumModal.jsx
+++ todo/src/components/custumModal.jsx
@@ -10,8 +10,8 @@ function custumModal({isOpen, onClose, onConfirm}) {
         <h4>Deseja excluir esse item?</h4>
         <h5>Colocar as descrições das tarefas aqui.</h5>
         <div className={styles.button}>
-        <button className={styles.btn-right} onClick={onClose}>Não</button>
-        <button className={styles.btn-left}  onClick={onConfirm}>Sim</button>
+        <button className={styles.btnLeft} onClick={onClose}>Não</button>
+        <button className={styles.btnRight}  onClick={onConfirm}>Sim</button>
         </div>
        
 
